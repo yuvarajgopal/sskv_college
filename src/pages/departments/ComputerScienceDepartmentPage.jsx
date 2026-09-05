9c186d638cebe775363beb59fb39a1c281894ed1
senthilkumar <senyuv@gmail.com>
2026-09-05 09:03:48 +0530

    sep05

--- src/pages/departments/ComputerScienceDepartmentPage.jsx
+++ src/pages/departments/ComputerScienceDepartmentPage.jsx
@@ -375,8 +375,8 @@ export default function ComputerScienceDepartmentPage() {
       </section>
       <DepartmentNewsletter
         departmentName="Computer Science"
-        pdfHrefOdd="/pdfs/newsletter/computer_s1.pdf"
-        pdfHrefEven="/pdfs/newsletter/computer_s2.pdf"
+        pdfHrefOdd="/pdfs/newsletter/cs_ns_sh1_odd.pdf"
+        pdfHrefEven="/pdfs/newsletter/cs_ns_sh1_even.pdf"
       />
 
 
